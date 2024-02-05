0802253c16bd6c8b32e9ef1c39341d3ca138a140
David_Barragan <david25barragan@gmail.com>
2024-02-04 22:20:21 -0500

    first commit

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+const Actor = require("./Actor");
+const Director = require("./Director");
+const Genre = require("./Genre");
+const Movie = require("./Movie");
+
+
+
+// tables pivot
+
+Movie.belongsToMany(Genre, {through: 'movieGenre'})
+Genre.belongsToMany(Movie, {through: 'movieGenre'})
+
+ Movie.belongsToMany(Actor, {through: 'movieActor'})
+ Actor.belongsToMany(Movie, {through: 'movieActor'})
+
+ Movie.belongsToMany(Director, {through: 'movieDirector'})
+ Director.belongsToMany(Movie, {through: 'movieDirector'})
